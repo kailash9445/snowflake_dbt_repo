a4eea6838d3432413bdfddc6b09e746bc15d0b36
KAILASH DARLA <Kailashdb94@gmail.com>
2025-03-22 18:00:37 +0000

    nnnn

--- mmodels/PERSONS_DETAILS_SAMPLE_FDR.sql
+++ mmodels/PERSONS_DETAILS_SAMPLE_FDR.sql
@@ -1,6 +1,7 @@
 --select * from {{source("kailash sources","PERSONS_SAMPLE_FDR")}}
-select *,
-{{(sales_percentage('sales1','sales2'))}} as change,
-{{(sales_percentage('sales1','sales3'))}} as change1,
-{{(sales_percentage('sales1','cost'))}} as change2
+*select *,
+{{(sales_percentage1('sales1','sales2'))}} as change,
+{{(sales_percentage1('sales1','sales3'))}} as change1,
+{{(sales_percentage1('sales1','cost'))}} as change2
 from {{source("kailash sources","PERSONS_SAMPLE_FDR")}}
+
