596b18a0eb1e6ac249f2aa723c4bc106ed8b2725
KAILASH DARLA <Kailashdb94@gmail.com>
2025-03-23 11:24:39 +0000

    hnh

--- mmodels/payment_modes.sql
+++ mmodels/payment_modes.sql
@@ -1,6 +1,5 @@
 
 {%- set j = dbt_utils.get_column_values (source("kailash sources","PAY_MODES"),'payment_mode') -%}
-
 select
 {%- for i in j %}
 sum(case when payment_mode =  '{{i}}'  then sales end) as {{i}}_SALES,
