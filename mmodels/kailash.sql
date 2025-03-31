80e2570a72c496b18e2b1d45b9dd27d3e44caf15
Kailash Darla <Kailashdb94@gmail.com>
2025-03-31 11:45:41 +0000

    commit

--- mmodels/kailash.sql
+++ mmodels/kailash.sql
@@ -1,2 +1,3 @@
-select c.* from DB_KAILASH.SCH_KAILASH.BKP_EMPLOYEE c  --country_codes  ##EMPLOYEE
+select * from DB_KAILASH.SCH_KAILASH.BKP_EMPLOYEE   --country_codes  ##EMPLOYEE
 
+--select c.* from DB_KAILASH.SCH_KAILASH.BKP_EMPLOYEE c
