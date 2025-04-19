468f3fa92a6bddb9372bef028951b78c638242d7
kailash9445 <kailashbabu.darla@gmail.com>
2025-04-19 21:26:32 +0530

    Update kailash.sql

--- mmodels/kailash.sql
+++ mmodels/kailash.sql
@@ -1,3 +1,8 @@
 select * from DB_KAILASH.SCH_KAILASH.BKP_EMPLOYEE   --country_codes  ##EMPLOYEE
 
 --select c.* from DB_KAILASH.SCH_KAILASH.BKP_EMPLOYEE c
+
+
+--select * from DB_KAILASH.SCH_KAILASH.EMPLOYEE   --country_codes  ##EMPLOYEE
+
+--select c.* from DB_KAILASH.SCH_KAILASH.EMPLOYEE_BKP c
