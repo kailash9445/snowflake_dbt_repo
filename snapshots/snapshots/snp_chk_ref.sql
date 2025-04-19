1f998d5fa5bbea820c5ae748f6b1d818320f8864
kailash9445 <kailashbabu.darla@gmail.com>
2025-04-19 21:18:05 +0530

    Create snp_chk_ref.sql

---
+++ snapshots/snapshots/snp_chk_ref.sql
@@ -0,0 +1,12 @@
+{%snapshot ads_snapshot_kailashh_chk_ref%}--DB_KAILASH.SNAPSHOTSSS.ADS_SNAPSHOT_KAILASHH_CHK_REF
+{{
+    config(
+      target_schema='snapshotsss',
+      unique_key = 'ENO',
+      strategy='check',
+      check_cols=['ENAME','JOB']
+      
+    )
+}}
+select * from {{ref('kailash')}} --DB_KAILASH.DB_KAILASH.kailash 
+{% endsnapshot %}
