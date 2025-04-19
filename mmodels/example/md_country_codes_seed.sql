f5b297771b1675ac374e784991f934fbac42647e
kailash9445 <kailashbabu.darla@gmail.com>
2025-04-19 21:28:47 +0530

    Create md_country_codes_seed.sql

---
+++ mmodels/example/md_country_codes_seed.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='table') }}
+
+with source_t_sample as (
+    select country_code as col1  from {{ ref('country_codes') }}
+),
+
+final as (
+    select * from source_t_sample
+)
+
+select * from final
+
+
+-- created with 1 column
+-- This refers to the table created from seeds/country_codes.csv
+--select * from {{ ref('country_codes') }}
