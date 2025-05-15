bb7d4aac33695f2e7e7c2f994369dd08e73e6e38
kailash9445 <kailashbabu.darla@gmail.com>
2025-05-15 20:48:41 +0530

    Update snp_chk_ref.sql

--- snapshots/snapshots/snp_chk_ref.sql
+++ snapshots/snapshots/snp_chk_ref.sql
@@ -1,9 +1,5 @@
 {%snapshot ads_snapshot_kailashh_chk_ref%}--DB_KAILASH.SNAPSHOTSSS.ADS_SNAPSHOT_KAILASHH_CHK_REF
 {{
-
-
-
-    
     config(
       target_schema='snapshotsss',
       unique_key = 'ENO',
