453c3f476a7d77979b1697c4e2c414e9225f69b2
babudkailash@gmail.com <babudkailash@gmail.com>
2025-05-15 15:37:32 +0000

    CMT

--- snapshots/snapshots/snp_chk_ref.sql
+++ snapshots/snapshots/snp_chk_ref.sql
@@ -1,6 +1,6 @@
 {%snapshot ads_snapshot_kailashh_chk_ref%}--DB_KAILASH.SNAPSHOTSSS.ADS_SNAPSHOT_KAILASHH_CHK_REF
 {{
-    config(HI HELLO HAI
+    config(HI HELLO HAI GURU
       target_schema='snapshotsss',
       unique_key = 'ENO',
       strategy='check',
