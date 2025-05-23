98b5d0e259251aa72e31249712940d83d8ee2fea
KAILASH9445 <babudkailash@gmail.com>
2025-05-23 11:40:08 +0000

    commit

--- mmodels/kailash.sql
+++ mmodels/kailash.sql
@@ -4,4 +4,4 @@ select * from DB_KAILASH.SCH_KAILASH.EMPLOYEE --country_codes  ##EMPLOYEE
 
 --select * from DB_KAILASH.SCH_KAILASH.EMPLOYEE   --country_codes  ##EMPLOYEE
 
---select c.* from DB_KAILASH.SCH_KAILASH.EMPLOYEE_BKP c kailash,kamalesh,naresh,srinivasulu
+--select c.* from DB_KAILASH.SCH_KAILASH.EMPLOYEE_BKP c kailash,kamalesh,naresh miltry family
