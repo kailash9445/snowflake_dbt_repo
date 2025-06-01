9218bf160381b0b3f54c96b1d441bd6c2119408d
kumar mv <kumarmv236@gmail.com>
2025-06-01 02:25:56 +0000

    c

--- mmodels/kamaleshwar.sql
+++ mmodels/kamaleshwar.sql
@@ -1,1 +1,19 @@
-select E.* from DB_KAILASH.SCH_KAILASH.EMPLOYEE_BKP E--country_codes  ##EMPLOYEE
+{# duplicates sql query group by,having clauses....#}
+
+select DPNO,count(*)
+
+from {{ref('kailash')}} 
+
+--GROUP BY dpno
+{{ dbt_utils.group_by(n=1) }}
+--group by 2  means group by second column
+--group by 1
+having count(*)>=5
+
+--earlier 3 records...10,20,30...after adding having clause 2 records.
+--ENO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DPNO
+
+{# output
+20     5
+30     6
+#}
