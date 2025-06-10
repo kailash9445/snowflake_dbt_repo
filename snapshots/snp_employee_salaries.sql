c815c25877b8371d359d271feeee64093f822d18
kumar mv <kumarmv236@gmail.com>
2025-06-10 10:02:31 +0000

    cc

--- snapshots/snp_employee_salaries.sql
+++ snapshots/snp_employee_salaries.sql
@@ -1,13 +1,13 @@
 {% snapshot snp_employee_salaries_snapshot %}
-{% set unique_key = 'eno' %}
+{% set uniq_key = 'eno' %}
 {% set strategy = 'check' %}
 {% set check_cols = ['ename', 'job'] %}
 {{ config(
     target_schema='snapshots',
-    unique_key=unique_key,
-    strategy=strategy,
-    check_cols=check_cols
-    updated_at='hiredate'
+    unique_key= uniq_key,
+    strategy= strategy,
+    check_cols= check_cols,
+    updated_at= 'hiredate'
 ) }}
 --TBL_emp_10_20
 --VW_emp_10_20
