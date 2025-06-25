e203ef1f2ad389a24cc67edac0f92fb359244ae1
kumar mv <kumarmv236@gmail.com>
2025-06-25 02:28:03 +0000

    commit_june25_2025

--- tests/tst_employee_hiredate.sql
+++ tests/tst_employee_hiredate.sql
@@ -17,5 +17,5 @@ tests:
       max_value: 10 # (Optional)
       row_condition: "id is not null" # (Optional)
       strictly: false # (Optional. Default is 'false'. Adds an 'or equal to' to the comparison operator for min/max)
-
+-
 #}
